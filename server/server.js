a0616071e068276a65a88f127cdbbb0dd8f83d4d
popelkin <shurik.wps@gmail.com>
2018-11-22 13:07:49 +0200

    Add POST todos

---
+++ server/server.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (request, response) => {
+    let todo = new Todo({
+        text: request.body.text
+    });
+
+    todo.save().then((doc) => {
+        response.send(doc);
+    }, (error) => {
+        response
+            .status(400)
+            .send(error)
+        ;
+    });
+});
+
+app.listen(3000, () => {
+    console.log(`Started on port 3000`);
+});
