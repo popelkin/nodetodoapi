a29c5bca146e83abf6fac421cc02518b5a88a4b1
popelkin <shurik.wps@gmail.com>
2018-11-25 20:58:37 +0200

    Bag testing

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,6 @@ if (env === 'development') {
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if (env === 'production') {
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
-mongoose.connect(process.env.MONGODB_URI).then().catch(() => {
+console.log('test');
+/*mongoose.connect(process.env.MONGODB_URI).then().catch(() => {
     mongoose.connect('mongodb://nodetodoapi:nodetodoapi123@ds111461.mlab.com:11461/nodetodoapi');
-});
-
+});*/
+console.log('test2');
 module.exports = {
     mongoose
 };
