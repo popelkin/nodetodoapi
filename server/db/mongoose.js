f33c5a7441252152d1001d7141157455b3b8812f
popelkin <shurik.wps@gmail.com>
2018-11-25 15:59:32 +0200

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,9 @@
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp').then(() => {
-    console.log('Connected to localhost');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
 }).catch(() => {
     mongoose.connect('mongodb://nodetodoapi:nodetodoapi123@ds111461.mlab.com:11461/nodetodoapi');
-    console.log('Connected to Heroku');
 });
 
 module.exports = {
