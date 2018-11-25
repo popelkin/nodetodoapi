8d14a32deaac48d25af35fbe45b32efc39dec81a
popelkin <shurik.wps@gmail.com>
2018-11-25 16:11:35 +0200

    Fixing bugs DB connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-}).catch(() => {
+mongoose.connect(process.env.MONGODB_URI).then().catch(() => {
     mongoose.connect('mongodb://nodetodoapi:nodetodoapi123@ds111461.mlab.com:11461/nodetodoapi');
 });
 
